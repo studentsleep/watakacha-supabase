efa62dc9d50434817cb2c4afccdecc495bd3a718
Thanawat <nicasio1144@gmail.com>
2026-02-08 03:52:23 +0700

    ver.ss7

---
+++ config/cloudinary.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Cloudinary settings. Cloudinary is a cloud
+    | service that offers a solution to a web application's entire image
+    | management pipeline.
+    |
+    */
+
+    'cloud_url' => env('CLOUDINARY_URL'),
+
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+
+    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the Cloudinary settings for your application.
+    |
+    */
+    'cloud' => [
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
+
+    'url' => [
+        'secure' => true,
+    ],
+
+];
