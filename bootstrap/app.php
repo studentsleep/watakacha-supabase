d71014a6204fab66327399a63c52b16caeb74794
Thanawat <nicasio1144@gmail.com>
2026-02-17 17:09:36 +0700

    ver.ss12

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,13 +6,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+
+        // 🟢 เพิ่มบรรทัดนี้เข้าไป เพื่อให้ Render ส่ง Cookie ได้สำเร็จ
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
